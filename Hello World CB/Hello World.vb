6bee2d47f5b2efad03a84a2fb8c53f33958a57ec
CL Bogart <clbogart3@outlook.com>
2022-01-12 07:54:56 -0700

    remove beeps

--- Hello World CB/Hello World.vb
+++ Hello World CB/Hello World.vb
@@ -7,9 +7,6 @@ Module Module1
     Sub Main()
         Console.WriteLine("Hello World")
         Console.ReadLine()
-        Console.Beep()
-        Console.Beep()
-        Console.Beep()
     End Sub
 
 End Module
